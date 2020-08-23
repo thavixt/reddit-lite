d0045c5cb8965eff368efa5cb3a4d58a9359d09c
thavixt <thavixt@users.noreply.github.com>
2020-08-23 15:34:35 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.79af3f8e168f6931fae1fd2b0626afd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8f7de61fbfbd4a4a9bbc9f2fb58a0f",
+    "url": "/reddit-lite/index.html"
+  },
+  {
+    "revision": "7d1e951627e85b692a98",
+    "url": "/reddit-lite/static/css/main.8eac5511.chunk.css"
+  },
+  {
+    "revision": "3b28d8de313b601bbe56",
+    "url": "/reddit-lite/static/js/2.47abe4d0.chunk.js"
+  },
+  {
+    "revision": "df6dc368aaa65f11a7a2c6c9ddec5cc4",
+    "url": "/reddit-lite/static/js/2.47abe4d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1e951627e85b692a98",
+    "url": "/reddit-lite/static/js/main.809fc115.chunk.js"
+  },
+  {
+    "revision": "cc7a8db4d2df305d2e32",
+    "url": "/reddit-lite/static/js/runtime-main.09cf6c25.js"
+  }
+]);
